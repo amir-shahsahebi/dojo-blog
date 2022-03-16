6b2befd242f0f2c6874561d97e194c802ec1f35c
amir shahsahebi <shahsahebiamir@gmail.com>
2022-03-16 23:27:38 +0330

    dynamic values completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@
 import "./App.css";
 
 function App() {
-  const title = "welcome to new blog  ";
+  const title = "welcome to new blog";
+  const like = 50;
+  const link = "http://www.google.com"
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
+        <p>liked {like} times</p>
+        <p>{20}</p>
+        <p>{"hello"}</p>
+        <p>{[7,8,9,10,11]}</p>
+        <p>{Math.random()*10}</p>
+        <a href={link}>google</a>
       </div>
     </div>
   );
