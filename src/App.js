28b5911a1b2213fab0f146b192ba004668cb5472
amir shahsahebi <shahsahebiamir@gmail.com>
2022-03-16 23:55:48 +0330

    Multiple components completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 // import logo from './logo.svg';
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   const title = "welcome to new blog";
   const like = 50;
-  const link = "http://www.google.com"
+  const link = "http://www.google.com";
   return (
     <div className="App">
+      <Navbar />
+      {/* <Navbar></Navbar> */}
       <div className="content">
-        <h1>{title}</h1>
+        <Home />
         <p>liked {like} times</p>
         <p>{20}</p>
         <p>{"hello"}</p>
-        <p>{[7,8,9,10,11]}</p>
-        <p>{Math.random()*10}</p>
+        <p>{[7, 8, 9, 10, 11]}</p>
+        <p>{Math.random() * 10}</p>
         <a href={link}>google</a>
       </div>
     </div>
