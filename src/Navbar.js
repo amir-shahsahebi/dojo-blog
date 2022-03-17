d9cc9cfdec06affbd27bf939f36c58cb1c7f86c3
amir shahsahebi <shahsahebiamir@gmail.com>
2022-03-18 00:21:31 +0330

    adding inline scc in jsx with {{}}

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,23 @@
 //sfc tab
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <h1>The dojo blog</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
-            </div>
-        </nav>
-      );
-}
- 
+  return (
+    <nav className="navbar">
+      <h1>The dojo blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a
+          href="/create"
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          New Blog
+        </a>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
