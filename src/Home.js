64362b0d7e3517b8c52cad161c02da87bb7ba222
amir shahsahebi <shahsahebiamir@gmail.com>
2022-03-19 16:06:35 +0330

    Click events added

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Homepage</h2>
-        </div>
-    );
-}
- 
+  const handleClick = () => {
+    console.log("hi, you click me");
+  };
+  const handleClickAgain = (name) => {
+    console.log("Hello " + name);
+  };
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain("Mario")}>Click me again</button>
+    </div>
+  );
+};
+
 export default Home;
