06125b56087f596746c613450f8903ccfe94f0df
amir shahsahebi <shahsahebiamir@gmail.com>
2022-03-19 16:36:39 +0330

    hook added

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 const Home = () => {
   const [name, setName] = useState("mario");
+  const [age, setAge] = useState("25")
   const handleClick = () => {
     // console.log("hi, you click me", e);
     setName("Amir");
+    setAge(28)
   };
   //   const handleClickAgain = (name, e) => {
   //     console.log("Hello " + name, e.target);
@@ -12,7 +14,7 @@ const Home = () => {
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <p>{name}</p>
+      <p>{name} is {age} years old </p>
       <button onClick={handleClick}>Click me</button>
       <button
       //    onClick={(e) => handleClickAgain("Mario", e)}
