ad88fd09178e2b1fae9b6df70d4c74a2876ace2a
amir shahsahebi <shahsahebiamir@gmail.com>
2022-03-23 02:22:13 +0430

    react router dom addded

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@
 // import "./App.css";
 import Navbar from "./Navbar";
 import Home from "./Home";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 function App() {
   const title = "welcome to new blog";
   const like = 50;
   const link = "http://www.google.com";
   return (
+    // all entire app need to soround bu Router and components nested in this app have access to Router 
+    <Router>
     <div className="App">
       <Navbar />
       {/* <Navbar></Navbar> */}
       <div className="content">
-        <Home />
+        {/* all of Routs go inside Switch component */}
+        <Switch>
+          {/* the path is any thing that come after home page url address */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
         <p>liked {like} times</p>
         <p>{20}</p>
         <p>{"hello"}</p>
@@ -21,6 +30,7 @@ function App() {
         <a href={link}>google</a>
       </div>
     </div>
+    </Router>
   );
 }
 
