b23e7bcc4b94545cf261de5dee9307f584203db3
amir shahsahebi <shahsahebiamir@gmail.com>
2022-03-23 15:03:03 +0430

    assing dynamic id with useParams

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 
 function App() {
   const title = "welcome to new blog";
@@ -24,6 +25,9 @@ function App() {
           </Route>
           <Route path="/create">
             <Create />
+          </Route>
+          <Route path="/blogs/:id">
+            <BlogDetails />
           </Route>
         </Switch>
         <p>liked {like} times</p>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,20 @@
 // const BlogList = (props) => {
+
+import { Link } from "react-router-dom";
+
 // or using distructure method
 const BlogList = ({blogs , title}) => {
 //   console.log(props, blogs);
 
   return (
     <div className="blog-list">
-        <h2>{title}</h2>
+      <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
+          <Link to={`/blogs/${blog.id}`}>
           <h2>{blog.title}</h2>
           <p>written by {blog.author}</p>
+          </Link>
           {/* <button onClick={()=>handleDelete(blog.id)}>delete blog</button> */}
         </div>
       ))}
