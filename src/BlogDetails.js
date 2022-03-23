7650b2dd0ce11eaaac8c5cf1c499786d3f71b49c
amir shahsahebi <shahsahebiamir@gmail.com>
2022-03-23 17:50:44 +0430

    deleting blog added

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,21 @@
 import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams() // in App.js we define path => /blogs/:id ===so===> the id need to be a variable so we use useParams and mention id that we assign that in app.js to set links dynamic and also use it in <h2></h2>
     const { data: blog, error, isPending } = useFetch("http://localhost:8000/blogs/" + id);
+    const history = useHistory()
+
+    const handleDelete = () => {
+        fetch("http://localhost:8000/blogs/"+ blog.id, {
+            method: "DELETE",
+
+        }).then(()=> {
+            history.push("/")
+        })
+    }
+
     return(
         <div className="blog-details">
         {isPending && <div>Loading ...</div>}
@@ -13,6 +25,7 @@ const BlogDetails = () => {
                 <h2>{blog.title}</h2>
                 <p>written by {blog.author}</p>
                 <div>{blog.body}</div>
+                <button onClick={handleDelete}>Delete</button>
             </article>
         )}
         
