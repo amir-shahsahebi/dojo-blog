75dddab428b36791816281312abdcfe7d9b4b927
amir shahsahebi <shahsahebiamir@gmail.com>
2022-03-23 15:47:30 +0430

    BlogDetails completed via useFetch an id of Params

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams() // in App.js we define path => /blogs/:id ===so===> the id need to be a variable so we use useParams and mention id that we assign that in app.js to set links dynamic and also use it in <h2></h2>
-
+    const { data: blog, error, isPending } = useFetch("http://localhost:8000/blogs/" + id);
     return(
         <div className="blog-details">
-        <h2>Blog Details - {id}</h2>
+        {isPending && <div>Loading ...</div>}
+        {error && <div>{error}</div>}
+        {blog && (
+            <article>
+                <h2>{blog.title}</h2>
+                <p>written by {blog.author}</p>
+                <div>{blog.body}</div>
+            </article>
+        )}
+        
         </div>
     );
 }
