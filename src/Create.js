35d2fdf756074f4f153ae471c1cc09a3bf531b76
amir shahsahebi <shahsahebiamir@gmail.com>
2022-03-23 17:16:45 +0430

    adding new blog with sending POST request

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,22 @@ const Create = ()=> {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('mario')
+    const [isPending, setIsPending] = useState(false) //because at the begining, we dont press submit :)
+
     const handleSubmit = (e) => {
         e.preventDefault()
         const blog = {title, body, author}
-        console.log(blog)
+        // console.log(blog)
+        setIsPending(true)
+
+        fetch("http://localhost:8000/blogs",{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            console.log('new blog added');
+            setIsPending(false)
+        })
     }
     return(
        <div className="create">
@@ -35,7 +47,8 @@ const Create = ()=> {
                     <option value="yoshi">yoshi</option>
                     <option value="amir">amir</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog ...</button>}
                 <p>{title}</p>
                 <p>{body}</p>
                 <p>{author}</p>
