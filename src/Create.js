6f2eada9b30026d8f1c19da56c1ee2705c0e65e4
amir shahsahebi <shahsahebiamir@gmail.com>
2022-03-23 17:37:25 +0430

    useHistory for redirecting to home page

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom"; // this use for redirecting page to home page    
 
 const Create = ()=> {
-    const [title, setTitle] = useState('')
+    const [title, setTitle] = useState('') //we use these for better way to sending data via fetch POST to json server
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('mario')
     const [isPending, setIsPending] = useState(false) //because at the begining, we dont press submit :)
+    const history = useHistory()
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -16,9 +18,11 @@ const Create = ()=> {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(blog)
-        }).then(()=>{
+        }).then(()=>{ // because ist a promise and await ... we use .then()
             console.log('new blog added');
             setIsPending(false)
+            // history(go -1) // it says that go to one page before but in this page we do not need it
+            history.push('/') // it redirect on home page bacause we assing "/" for home page in App.js
         })
     }
     return(
@@ -49,9 +53,9 @@ const Create = ()=> {
                 </select>
                 {!isPending && <button>Add Blog</button>}
                 {isPending && <button disabled>Adding Blog ...</button>}
-                <p>{title}</p>
+                {/* <p>{title}</p>
                 <p>{body}</p>
-                <p>{author}</p>
+                <p>{author}</p> */}
            </form>
        </div> 
     );
